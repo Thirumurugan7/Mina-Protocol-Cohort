a173a0d513a1262b1bbc63c0d8883b8479769860
thirumurugan7 <thiru82003@gmail.com>
2024-01-07 12:09:40 +0530

    mina proof generated

---
+++ src/Square.ts
@@ -0,0 +1,17 @@
+import { Field, SmartContract, state, State, method } from 'o1js';
+
+export class Square extends SmartContract {
+  @state(Field) num = State<Field>();
+
+  init() {
+    super.init();
+    this.num.set(Field(3));
+  }
+
+  @method update(square: Field) {
+    const currentState = this.num.get();
+    this.num.requireEquals(currentState);
+    square.assertEquals(currentState.add(currentState));
+    this.num.set(square);
+  }
+}
